5692dd8de3bb6a59653fd2ba10eb006c3cea7da1
Thanoon Younes <engthanoon1@gmail.com>
2023-09-29 13:50:31 +0300

    first upload files

--- tictok.py
+++
@@ -1,7 +0,0 @@
-echo "# tic-tok-Pass-captcha" >> README.md
-git init .
-git add .
-git commit -m "first upload files"
-git branch -M main
-git remote add origin git@github.com:Thanoon12k/tic-tok-Pass-captcha.git
-git push -u origin main
